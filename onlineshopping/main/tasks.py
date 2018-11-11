228feffb5160472200489f240c63ff1ad579ebb4
Asmaa <asmaa.hassan@smart-medicalservices.com>
2018-11-11 22:20:23 +0200

    remove unwanted code

--- onlineshopping/main/tasks.py
+++ onlineshopping/main/tasks.py
@@ -7,8 +7,8 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 
-@periodic_task(run_every=timedelta(seconds=100))
-def crawl_task():
+@periodic_task(run_every=timedelta(seconds=30))
+def crawl_domain():
     print("crawl_task")
     from .crawl import domain_crawl
     return domain_crawl()
--- onlineshopping/scrapy_app/scrapy_app/spiders/jumiaSpider.py
+++ onlineshopping/scrapy_app/scrapy_app/spiders/jumiaSpider.py
@@ -10,25 +10,6 @@ from main.models import ScrapyItem
 
 class JumiaSpider(CrawlSpider):
     name = 'jumiaspider'
-    # allowed_domains = ['https://google.com']
-    # start_urls = ['http://https://google.com/']
-    #
-    # rules = (
-    #     Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    # )
-    #
-    # def parse_item(self, response):
-    #     i = {}
-    #     #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-    #     #i['name'] = response.xpath('//div[@id="name"]').extract()
-    #     #i['description'] = response.xpath('//div[@id="description"]').extract()
-    #     return i
-
-    # allowed_domains = ["bbc.com"]
-    # , 'https://www.jumia.com.eg/deal-of-the-day/'
-    # allowed_domains = ['https://deals.souq.com/eg-en/']
-    # start_urls = ['http://bbc.com/']
-    # start_urls = ['https://deals.souq.com/eg-en/']
     allowed_domains = ['jumia.com.eg']
     start_urls = ['https://www.jumia.com.eg/deal-of-the-day/']
 
